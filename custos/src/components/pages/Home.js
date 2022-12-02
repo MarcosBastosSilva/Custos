a9621164b1ddd3eb25883a97bc121f7c9ccea780
Marcos Vinicius Bastos da Silva <bastosmarcos90@gmail.com>
2022-12-01 21:25:39 -0300

    update

--- custos/src/components/pages/Home.js
+++ custos/src/components/pages/Home.js
@@ -1,5 +1,16 @@
+import styles from './Home.module.css'
+import savings from '../../img/savings.svg'
+import LinkButton from '../Layout/LinkButton'
+
 function Home (){
-    return <p>Home</p>
+    return (
+        <section className={styles.home_container}>
+            <h1>Bem-Vindo ao <span>Custos</span></h1>
+            <p>Comece a gerenciar os seus próprios projetos agora mesmo.</p>
+            <LinkButton to="/NovoProjeto" text="Criar Projeto"/>
+            <img src={savings} alt="Custos"/>
+        </section>
+    )
 }
 
-export default Home
+export default Home 
--- custos/src/components/pages/NovoProjeto.js
+++ custos/src/components/pages/NovoProjeto.js
@@ -1,5 +1,14 @@
+import styles from './NovoProjeto.module.css'
+import ProjetctForm from '../Project/ProjectForm'
+
 function NovoProjeto (){
-    return <p>NovoProjeto</p>
+    return (
+        <div className={styles.newproject_container}>
+            <h1>Criar Projeto</h1>
+            <p>Crie seu Projeto, e após isso adicione os serviços desejados!</p>
+            <ProjetctForm/>
+        </div>
+    )
 }
 
 export default NovoProjeto
